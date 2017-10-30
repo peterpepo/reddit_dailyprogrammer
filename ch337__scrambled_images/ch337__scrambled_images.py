f7571ec04e93af29620c522dfb0e0080ad95d6d6
Peter Pepo <peterpepo1@gmail.com>
2017-10-30 22:11:58 +0100

    Challenge #337 Scrambled images - Refactor
    * Refactors solution for Challenge #337 Scrambled images
    ** Solution works, but colored pixel is aligned to the left, this need to be fixed

--- ch337__scrambled_images/ch337__scrambled_images.py
+++ ch337__scrambled_images/ch337__scrambled_images.py
@@ -1,7 +1,42 @@
 from PIL import Image
 
+
+def is_grey(pixel):
+    """
+    Returns true if passed pixel is grey, otherwise returns false.
+    """
+    return pixel[0] == pixel[1] == pixel[2]
+
+
+def find_colored(image):
+    """
+    Finds first non-grey pixel.
+    """
+    image_pixels = image.getdata()
+
+    i=0
+    while is_grey(image_pixels[i]):
+        i += 1
+
+    return i
+
+
+def roll_image(image, amount):
+    image_width, image_height = image.size
+
+    amount = amount % image_width
+    new_left_part = image.crop((0, 0, amount, image_height))
+    new_right_part = image.crop((amount, 0, image_width, image_height))
+
+    new_image = Image.new(image.mode, image.size)
+    new_image.paste(new_right_part, (0,0))
+    new_image.paste(new_left_part, (image_width-amount, 0))
+
+    return new_image
+
 image_name = "test_rMYBq14.PNG"
-output_image_name = image_name.split(".")[0] + "_UNSCRAMBLED_." + image_name.split(".")[1]
+
+output_image_name = image_name.split(".")[0] + "_UNSCRAMBLED." + image_name.split(".")[1]
 
 source_image = Image.open(image_name)
 
@@ -10,35 +45,14 @@ image_width, image_height = source_image.size
 new_image = Image.new(source_image.mode, source_image.size)
 
 # Cycle through image rows
-for image_line_index in range(image_height-1):
+for image_line_index in range(image_height):
 
     # Chop off single line of image
     image_line = source_image.crop((0, image_line_index, image_width, image_line_index+1))
 
-    roll_amount = 10
-    # Find colored pixel in the line
-    image_line_pixels = image_line.getdata()
-    for current_pixel_index in range(len(image_line_pixels)):
-        current_pixel = image_line_pixels[current_pixel_index]
-        is_grey = current_pixel[0] == current_pixel[1] == current_pixel[2]
+    roll_amount = find_colored(image_line)
+    image_line = roll_image(image_line, roll_amount)
 
-        if not(is_grey):
-            roll_amount = current_pixel_index
-
-
-    # print(image_line)
-    # Roll line
-
-    roll_amount = roll_amount % image_width
-
-    new_image_line_part1 = source_image.crop((0, image_line_index, roll_amount, image_line_index+1))
-    new_image_line_part2 = source_image.crop((roll_amount, image_line_index, image_width, image_line_index+1))
-    new_image_line_part1.load()
-    new_image_line_part2.load()
-
-    new_image.paste(new_image_line_part2, (0, image_line_index, image_width-roll_amount, image_line_index+1))
-    new_image.paste(new_image_line_part1, (image_width-roll_amount, image_line_index, image_width, image_line_index+1))
-
-    # new_image.paste(image_line, (0, image_line_index, image_width, image_line_index+1))
+    new_image.paste(image_line, (0, image_line_index))
 
 new_image.save(output_image_name, "PNG")
